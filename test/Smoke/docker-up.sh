5801fba17a2d10c27f7ce7012220c6561cfffaad
Matt Howlett <matt.howlett@gmail.com>
2019-03-07 13:55:49 -0800

    advertsed listeners

--- test/Smoke/docker-up.sh
+++ test/Smoke/docker-up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./zookeeper/docker-up.sh
-./kafka/docker-up.sh
-./schemaregistry/docker-up.sh
-./prometheus/docker-up.sh
+ADVERTISED_IP=$ADVERTISED_IP ./zookeeper/docker-up.sh
+ADVERTISED_IP=$ADVERTISED_IP ./kafka/docker-up.sh
+ADVERTISED_IP=$ADVERTISED_IP ./schemaregistry/docker-up.sh
+ADVERTISED_IP=$ADVERTISED_IP ./prometheus/docker-up.sh
