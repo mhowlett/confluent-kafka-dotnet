6db3a62ae887e76dd461cdb06caa632355ea9fd3
Matt Howlett <matt.howlett@gmail.com>
2019-03-08 09:33:33 -0800

    dsf

--- env/test.sh
+++ env/test.sh
@@ -2,6 +2,14 @@
 
 docker volume create confluent_kafka_dotnet
 docker run -it \
+    --name=build \
+    --network=host \
+    --rm \
+    -v confluent_kafka_dotnet:/git \
+    mcr.microsoft.com/dotnet/core/sdk:2.1.504-stretch \
+    bash -c "cd /git; git clone --branch $BRANCH https://github.com/$REPO/confluent-kafka-dotnet.git; cd confluent-kafka-dotnet; dotnet build; exit"
+
+docker run -it \
     --name=test \
     --network=host \
     --rm \
