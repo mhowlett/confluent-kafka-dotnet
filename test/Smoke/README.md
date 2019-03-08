5aa06b0052d91caeb6130fd029655dfc7f317b3d
Matt Howlett <matt.howlett@gmail.com>
2019-03-07 18:14:33 -0800

    interval

---
+++ test/Smoke/README.md
@@ -0,0 +1,22 @@
+
+
+
+Scripts that set up:
+1. A 3 broker kafka cluster (single zookeeper instance).
+2. 
+
+
+## Windows
+
+Kafka does not run reliably under Windows (whether using the .bat scripts included in the distribution,
+Linux subsystem for Windows or Docker for Windows). To set up a test cluster on Windows you'll need to
+run Linux in a virtual machine, such as [Virtual Box](https://www.virtualbox.org/wiki/Downloads). A 
+recipe for doing this is provided below.
+
+1. Download and install [Virtual Box](https://www.virtualbox.org/wiki/Downloads)
+2. Download
+
+## Linux 
+
+## MacOS
+
--- test/Smoke/prometheus/docker-up.sh
+++ test/Smoke/prometheus/docker-up.sh
@@ -6,9 +6,10 @@ docker run -d \
     --name=prometheus \
     --network=host \
     -v prometheus:/data/prometheus \
-    -e SCRAPE_INTERVAL="2s" -e EVAL_INTERVAL="2s" \
+    -e SCRAPE_INTERVAL="30s" -e EVAL_INTERVAL="30s" \
     -e ZK_IP=$ADVERTISED_IP -e ZK_METRICS_PORT=7062 \
     -e BROKER_1_IP=$ADVERTISED_IP -e BROKER_1_METRICS_PORT=7071 \
     -e BROKER_2_IP=$ADVERTISED_IP -e BROKER_2_METRICS_PORT=7072 \
     -e BROKER_3_IP=$ADVERTISED_IP -e BROKER_3_METRICS_PORT=7073 \
-    confluentinc/dotnet_test_prometheus:1
+    confluentinc/dotnet_test_prometheus:1 \
+    sleep 30; /opt/bootstrap.sh
