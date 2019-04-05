e81dd161a6f051c7e59fe3f8e53c869bbea95f8a
Matt Howlett <matt.howlett@gmail.com>
2019-04-05 08:34:34 -0700

    sdf

--- examples/ProducerBlog_StreamProcess/Program.cs
+++ examples/ProducerBlog_StreamProcess/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
+using Confluent.Kafka.Admin;
 using Bogus;
 using Bogus.DataSets;
 
@@ -11,18 +12,56 @@ namespace ProducerBlog_StatelessProcessing
 {
     class Program
     {
+        async static Task RecreateTopicsAsync(
+            string brokerAddress,
+            string weblogTopic,
+            string piiCompliantTopic,
+            string aggregatedTopic,
+            string windowOffsetTopic)
+        {
+            using (var adminClient = new AdminClientBuilder(new AdminClientConfig { BootstrapServers = brokerAddress }).Build())
+            {
+                try
+                {
+                    await adminClient.DeleteTopicsAsync(new [] { weblogTopic, piiCompliantTopic, aggregatedTopic, windowOffsetTopic });
+                }
+                catch (DeleteTopicsException ex)
+                {
+                    foreach (var r in ex.Results)
+                    {
+                        if (r.Error.Code != ErrorCode.UnknownTopicOrPart)
+                        {
+                            throw;
+                        }
+                    }
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(2));
+
+                await adminClient.CreateTopicsAsync(new []
+                    {
+                        // set replication factor to 1 so can work on single broker test cluster. typically set to 3.
+                        new TopicSpecification { Name = weblogTopic, NumPartitions = 24, ReplicationFactor = 1 },
+                        new TopicSpecification { Name = piiCompliantTopic, NumPartitions = 24, ReplicationFactor = 1 },
+                        new TopicSpecification { Name = aggregatedTopic, NumPartitions = 2, ReplicationFactor = 1 },
+                        new TopicSpecification { Name = windowOffsetTopic, NumPartitions = 1, ReplicationFactor = 1 }
+                    });
+            }
+        }
+
         async static Task Main(string[] args)
         {
-            if (args.Length != 3)
+            if (args.Length != 5)
             {
-                Console.WriteLine("usage: .. <bootstrap servers> <weblog topic> <processed topic>");
+                Console.WriteLine("usage: .. <bootstrap servers> <weblog topic> <processed topic> <aggregated topic> <window offset topic>");
                 Environment.Exit(1);
             }
 
             var brokerAddress = args[0];
             var weblogTopic = args[1];
             var piiCompliantTopic = args[2];
-            var aggregatedTopic = args[3];
+            var countryCountTopic = args[3];
+            var windowOffsetTopic = args[4];
 
             CancellationTokenSource cts = new CancellationTokenSource();
             Console.CancelKeyPress += (_, e) => {
@@ -30,16 +69,29 @@ namespace ProducerBlog_StatelessProcessing
                 cts.Cancel();
             };
 
-            // generate fake weblog data.
-            _ = Task.Run(async () => await WeblogSimulator.Generate(brokerAddress, weblogTopic, cts.Token));
+            await RecreateTopicsAsync(brokerAddress, weblogTopic, piiCompliantTopic, countryCountTopic, windowOffsetTopic);
+
+            // In a production system, each task (thread) below would correspond
+            // to a separate process.
+            var microservices = new List<Task>();
+
+            // generate some fake weblog data.
+            microservices.Add(Task.Run(async () => await WeblogSimulator.Generate(brokerAddress, weblogTopic, cts.Token)));
 
             // (mock) geoip lookup, remove pii information (IP address), and repartition by country.
-            _ = Task.Run(() => StatelessProcessor.Run(brokerAddress, weblogTopic, piiCompliantTopic, cts.Token));
+            microservices.Add(Task.Run(() => StatelessProcessor.Run(brokerAddress, weblogTopic, piiCompliantTopic, 1, cts.Token)));
+            // microservices.Add(Task.Run(() => StatelessProcessor.Run(brokerAddress, weblogTopic, piiCompliantTopic, 2, cts.Token)));
 
             // count the number of hits per country in 1hr time windows.
-            _ = Task.Run(() => TimeWindowAggregator.Run(brokerAddress, piiCompliantTopic, aggregatedTopic, cts.Token));
+            microservices.Add(Task.Run(() => TimeWindowAggregator.Run(brokerAddress, piiCompliantTopic, countryCountTopic, windowOffsetTopic, 1, cts.Token)));
+            // microservices.Add(Task.Run(() => TimeWindowAggregator.Run(brokerAddress, piiCompliantTopic, aggregatedTopic, 2, cts.Token)));
 
-            await Task.Delay(TimeSpan.MaxValue, cts.Token);
+            var result = await Task.WhenAny(microservices);
+
+            if (result.IsFaulted)
+            {
+                throw result.Exception;
+            }
         }
     }
 }
--- examples/ProducerBlog_StreamProcess/StatelessProcessor.cs
+++ examples/ProducerBlog_StreamProcess/StatelessProcessor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Confluent.Kafka;
 
 
@@ -10,7 +11,7 @@ namespace ProducerBlog_StatelessProcessing
         static IProducer<string, string> producer;
         static IConsumer<Null, string> consumer;
 
-        static void Process(ConsumeResult<Null, string> consumeResult, string outputTopic)
+        async static Task Process(ConsumeResult<Null, string> consumeResult, string outputTopic)
         {
             while (true)
             {
@@ -18,15 +19,27 @@ namespace ProducerBlog_StatelessProcessing
                 {
                     var logline = consumeResult.Value;
                     var firstSpaceIndex = logline.IndexOf(' ');
+                    if (firstSpaceIndex < 0)
+                    {
+                        throw new Exception("");
+                    }
                     var ip = logline.Substring(0, firstSpaceIndex);
-                    var country = MockGeoLookup.GetCountryFromIP(ip);
-                    var anonymizedLogline = logline.Substring(firstSpaceIndex);
-
+                    var country = await MockGeoLookup.GetCountryFromIPAsync(ip);
+                    var loglineWithoutIP = logline.Substring(firstSpaceIndex+1);
+                    var dateStart = loglineWithoutIP.IndexOf('[');
+                    var dateEnd = loglineWithoutIP.IndexOf(']');
+                    if (dateStart < 0 || dateEnd < 0 || dateEnd < dateStart)
+                    {
+                        throw new Exception("");
+                    }
+                    var requestInfo = loglineWithoutIP.Substring(dateEnd);
+                    
                     producer.BeginProduce(
                         outputTopic,
-                        new Message<string, string> { Key = country, Value = anonymizedLogline },
+                        new Message<string, string> { Key = country, Value = requestInfo, Timestamp = new Timestamp(DateTime.UtcNow, TimestampType.CreateTime) },
                         dr =>
                         {
+                            Console.WriteLine($"wrote: {requestInfo}");
                             // closure!
                             consumer.StoreOffset(consumeResult);
                         });
@@ -48,34 +61,78 @@ namespace ProducerBlog_StatelessProcessing
             string brokerAddress,
             string weblogTopic,
             string outputTopic,
+            int instanceId,
             CancellationToken cancellationToken)
         {
             var cConfig = new ConsumerConfig
             {
+                ClientId = $"pii-country-proc-consumer-{instanceId}",
                 GroupId = "weblog-example",
                 BootstrapServers = brokerAddress,
                 EnableAutoCommit = true,
                 EnableAutoOffsetStore = false,
-                AutoOffsetReset = AutoOffsetReset.Earliest
+                AutoOffsetReset = AutoOffsetReset.Latest
             };
 
             var pConfig = new ProducerConfig
             {
+                ClientId = $"pii-country-proc-producer-{instanceId}",
                 BootstrapServers = brokerAddress,
                 EnableIdempotence = true,
                 LingerMs = 5,
                 DeliveryReportFields = "none"
             };
 
+
+            bool processedMessage = false;
+
             using (producer =
                 new ProducerBuilder<string, string>(pConfig)
                     .SetErrorHandler((_, e) =>
                     {
-                        // idempotent fatal error.
+                        if (e.IsFatal)
+                        {
+                            // can these exceptions be caught?
+                            throw new KafkaException(e);
+
+                            // idempotent fatal error.
+                            //   - another case if different replicas are deleting log segments due to 
+                            //     retention at different speeds. low watermark offset changes on differnt replicas.
+                            //   - what if bad message, then maybe not guaraneed 1m not produced.
+                        }
+
+                        if (e.Code == ErrorCode.Local_AllBrokersDown ||
+                            e.Code == ErrorCode.Local_Authentication)
+                        {
+                            if (!processedMessage)
+                            {
+                                throw new KafkaException(e);
+                            }
+                        }
+
+                        // anything else best thought of as informational.
+                        // perhaps rates - if there's problems then quit? 
                     })
                     .Build())
+
             using (consumer =
                 new ConsumerBuilder<Null, string>(cConfig)
+                    .SetErrorHandler((_, e) =>
+                    {
+                        if (e.Code == ErrorCode.Local_AllBrokersDown ||
+                            e.Code == ErrorCode.Local_Authentication)
+                        {
+                            if (!processedMessage)
+                            {
+                                throw new KafkaException(e);
+                            }
+                        }
+                    })
+                    // failure to produce offsets.
+                    // measure eveything by rte
+                    //  consumption rate drops below 1 ten something wrong
+                    //  also what if # commit failures starting to reach input rae, can't comit.
+                    //  offset commits on own connection.
                     .Build())
             {
                 consumer.Subscribe(weblogTopic);
@@ -88,16 +145,21 @@ namespace ProducerBlog_StatelessProcessing
                         {
                             var cr = consumer.Consume(cancellationToken);
                             Process(cr, outputTopic);
+                            processedMessage = true;
                         }
                         catch (ConsumeException ex)
                         {
                             if (ex.Error.Code == ErrorCode.Local_ValueDeserialization)
                             {
-                                // bad data.
                                 continue;
                             }
+
                             // what else?
-                            // microservices we want to fail fast and restart the microservice.
+                            //  - group coordinator error.
+                            //  - any error you don't recognise should be ignored.
+                            //  - microservices we want to fail fast and restart the microservice.
+                            // I think we want to ignore:
+                            continue;
                         }
                     }
                 }
